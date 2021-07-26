d84a192733f2a43bb190475e8947ececc12f3958
Jonathan Ma <jonathanjma03@gmail.com>
2021-07-26 02:47:51 -0700

    more presets

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,11 +1,16 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class GameOfLife extends Application {
@@ -20,6 +25,50 @@ public class GameOfLife extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        Pane startPane = new Pane();
+        mainPane.setStyle("-fx-background: rgb(255,255,255);");
+
+        VBox column = new VBox(20);
+        column.setLayoutX(250); column.setLayoutY(70);
+        column.setAlignment(Pos.CENTER);
+
+        Label title = new Label("Conway's Game of Life");
+        title.setFont(Font.font(50));
+        Label instructions = new Label("Choose an option below to get started");
+        instructions.setFont(Font.font(20));
+
+        Button playground = new Button("Playground Mode");
+        playground.setFont(Font.font(15));
+        playground.setOnMouseClicked(e -> {
+            startGame();
+        });
+
+        Label or = new Label("or"); or.setFont(Font.font(20));
+        Label preset = new Label("Choose a preset below:");
+        preset.setFont(Font.font(20));
+
+        ObservableList<String> presetList = FXCollections.observableArrayList();
+        presetList.addAll("1", "2", "3", "4", "5");
+
+        ListView<String> presets = new ListView<>(presetList);
+        presets.setPrefWidth(300); presets.setPrefHeight(300);
+        presets.getSelectionModel().selectedItemProperty().addListener((observed_val, old_val, selected_val) -> {
+            System.out.println(selected_val);
+        });
+
+        column.getChildren().addAll(title, instructions, playground, or, preset, presets);
+        startPane.getChildren().add(column);
+        mainPane.setCenter(startPane);
+
+        Scene scene = new Scene(mainPane, size, size);
+        primaryStage.setTitle("The Game of Life");
+        primaryStage.getIcons().add(new Image("icon.png"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void startGame() {
 
         Pane boardPane = new Pane();
         boardPane.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
@@ -44,9 +93,9 @@ public class GameOfLife extends Application {
                 startStopBtn.setText("Start");
             }
         });
-        primaryStage.setOnCloseRequest(e -> {
+        /*primaryStage.setOnCloseRequest(e -> {
             if (animatorThread != null) {animator.setStopped(true);}
-        });
+        });*/
         Button nextBtn = new Button("Next Stage");
         nextBtn.setOnMouseClicked(e -> animator.nextStage());
         Button resetBtn = new Button("Reset");
@@ -60,20 +109,22 @@ public class GameOfLife extends Application {
 
         mainPane.setCenter(boardPane);
         mainPane.setBottom(infoBox);
-
-        Scene scene = new Scene(mainPane, size, size);
-        primaryStage.setTitle("The Game of Life");
-        primaryStage.getIcons().add(new Image("icon.png"));
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     public void loadAllPresets() {
         try {
-            animator.loadPreset(Preset.gosperGliderGun(30, 50, 0));
-            animator.loadPreset(Preset.gosperGliderGun(45, 43, 1));
-            animator.loadPreset(Preset.gosperGliderGun(37, 13, 2));
-            animator.loadPreset(Preset.gosperGliderGun(20, 21, 3));
+//            animator.loadPreset(Preset.gosperGliderGun(50, 50, 0));
+//            animator.loadPreset(Preset.gosperGliderGun(45, 30, 2));
+//            animator.loadPreset(Preset.rPentomino(50, 50, 0));
+//            animator.loadPreset(Preset.basicLife(20, 30, 0));
+//            animator.loadPreset(Preset.gliders(50, 30, 0));
+//            animator.loadPreset(Preset.acorn(60, 50, 0));
+//            animator.loadPreset(Preset.spider(50, 70, 0));
+//            animator.loadPreset(Preset.spider(76, 70, 5));
+//            animator.loadPreset(Preset.copperhead(50, 50, 0));
+//            animator.loadPreset(Preset.tannerP46(30, 30, 0));
+//            animator.loadPreset(Preset.simkinGliderGun(50, 50, 0));
+            animator.loadPreset(Preset.snark(50, 50, 0));
         } catch (IndexOutOfBoundsException ex) {ex.printStackTrace();}
     }
 }
--- Preset.java
+++ Preset.java
@@ -6,9 +6,373 @@ public class Preset {
     private static ArrayList<int[]> cells;
 
     // 0 deg = 0, 90 deg = 1, 180 deg = 2, 270 deg = 3
+    public static ArrayList<int[]> sample(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> snark(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+
+        addCell(-1, 3);
+        addCell(0, -1);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(0, -1);
+        addCell(0, -1);
+        addCell(0, -1);
+        addCell(1, 0);
+        addCell(1, 1);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 1);
+
+        addCell(1, -1);
+        addCell(0, -1);
+        addCell(0, -1);
+        addCell(1, 0);
+        addCell(-1, -1);
+        addCell(0, -1);
+        addCell(-1, -1);
+        addCell(-1, 0);
+        addCell(-1, 0);
+        addCell(0, -1);
+        addCell(-1, 0);
+        addCell(-1, 1);
+        addCell(0, 1);
+
+        addCell(-1, 8);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+
+        addCell(-10, -2);
+        addCell(1, 0);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(0, -1);
+
+        addCell(0, 9);
+        addCell(-1, 1);
+        addCell(2, -1);
+        addCell(0, 1);
+        addCell(0, 1);
+
+        addCell(8, -3);
+        addCell(1, 0);
+        addCell(-1, 1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(0, 1);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> simkinGliderGun(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+        addCell(6, -1);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+        addCell(-4, 2);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+
+        addCell(16,6);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(-1, -3);
+        addCell(1, 0);
+        addCell(2, 0);
+        addCell(1, 0);
+        addCell(1, 1);
+        addCell(1, 1);
+        addCell(-1, 1);
+        addCell(-1, 1);
+        addCell(5, -2);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+
+        addCell(-12, 5);
+        addCell(1, 0);
+        addCell(-1, 1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(0, 1);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> tannerP46(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(1, 0);
+        addCell(-1, 1);
+        addCell(1, 1);
+        addCell(0, 1);
+        addCell(-1, 0);
+
+        addCell(0, 6);
+        addCell(0, 1);
+        addCell(-1, -1);
+        addCell(0, 1);
+        addCell(-1, 1);
+        addCell(1, 1);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(0, 1);
+
+        addCell(0, 8);
+        addCell(-1, 0);
+        addCell(0, 1);
+        addCell(1, 0);
+
+        addCell(3, 0);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(1, 1);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 0);
+
+        addCell(1, -20);
+        addCell(1, 0);
+        addCell(-1, 1);
+        addCell(1, 1);
+        addCell(0, 1);
+        addCell(-1, 0);
+
+        addCell(1, 2);
+        addCell(0, 1);
+        addCell(-1, -1);
+        addCell(-1, 1);
+        addCell(1, 1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(0, 1);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> copperhead(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(1, 0);
+        addCell(3, 0);
+        addCell(1, 0);
+        addCell(-3, 1);
+        addCell(1, 0);
+        addCell(0, 1);
+        addCell(-1, 0);
+        addCell(-1, 1);
+        addCell(3, 0);
+
+        addCell(-5, 0);
+        addCell(0, 1);
+        addCell(0, 2);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(0, 1);
+        addCell(1, 0);
+
+        addCell(0, 2);
+        addCell(0, 1);
+        addCell(1, 0);
+        addCell(0, -1);
+
+        addCell(0, -2);
+        addCell(1, 0);
+        addCell(0, -1);
+        addCell(1, 0);
+        addCell(1, -1);
+        addCell(0, -2);
+        addCell(0, -1);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> spider(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(-2, 1);
+
+        addCell(1, 2);
+        addCell(0, 1);
+        addCell(1, 0);
+        addCell(0, -1);
+        addCell(2, 1);
+        addCell(1, 0);
+        addCell(0, 1);
+
+        addCell(-2, -6);
+        addCell(1, 0);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 0);
+        addCell(1, -1);
+        addCell(0, -1);
+        addCell(0, -1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(0, -1);
+        addCell(1, -1);
+        addCell(1, 1);
+        addCell(1, 0);
+
+        addCell(1, 2);
+        addCell(0, 1);
+        addCell(0, 1);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> acorn(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(1, 0);
+        addCell(0, -2);
+        addCell(2, 1);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> gliders(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(-1, 0);
+        addCell(-1, 0);
+        addCell(0, 1);
+        addCell(1, 1);
+
+        addCell(0, 15);
+        addCell(-3, 0);
+        addCell(-1, 1);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(1, 0);
+        addCell(1, -1);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> basicLife(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(1, 0);
+        addCell(0, 1);
+        addCell(-1, 0);
+
+        addCell(5, 7);
+        addCell(0, 1);
+        addCell(1, -1);
+        addCell(1, 0);
+
+        addCell(5, 7);
+        addCell(0, 1);
+        addCell(0, 1);
+        addCell(0, 1);
+
+        addCell(5, 7);
+        addCell(1, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+
+        addCell(5, 10);
+        addCell(-1, 1);
+        addCell(1, 0);
+        addCell(1, 0);
+
+        addCell(5, 10);
+        addCell(1, 0);
+        addCell(1, -1);
+        addCell(-1, 2);
+
+        addCell(20, -50);
+        addCell(1, 0);
+        addCell(1, 1);
+        addCell(-1, 1);
+        addCell(-1, -1);
+
+        addCell(0, 10);
+        addCell(1, 0);
+        addCell(1, 1);
+        addCell(-1, 1);
+        addCell(-1, 1);
+        addCell(-1, -1);
+        addCell(0, -1);
+
+        addCell(0, 10);
+        addCell(1, 0);
+        addCell(-1, 1);
+        addCell(2, 0);
+        addCell(0, 1);
+        addCell(-1, 0);
+
+        return cells;
+    }
+
+    public static ArrayList<int[]> rPentomino(int stX, int stY, int rotateCode) {
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
+        cells = new ArrayList<>();
+
+        addCell(0, 0);
+        addCell(-1, 0);
+        addCell(0, 1);
+        addCell(-1, 0);
+        addCell(1, 1);
+        return cells;
+    }
+
     public static ArrayList<int[]> gosperGliderGun(int stX, int stY, int rotateCode) {
-        Preset.x = stX; Preset.y = stY;
-        Preset.rotateCode = rotateCode;
+        Preset.x = stX; Preset.y = stY; Preset.rotateCode = rotateCode;
         cells = new ArrayList<>();
 
         addCell(0,0);
@@ -69,7 +433,12 @@ public class Preset {
         } else if (rotateCode == 3) {
             y += deltaX;
             x -= deltaY;
-        } else {throw new IllegalArgumentException("rotate code invalid");}
+        } else if (rotateCode == 5) {
+            y += deltaY;
+            x -= deltaX;
+        } else {
+            throw new IllegalArgumentException("rotate code invalid");
+        }
         cells.add(new int[]{y, x});
     }
 }
